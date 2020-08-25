296f28caf9bd88c86cf0bed52d5345cab4528c56
Kacper Grzeszczyk <kacgrzes@gmail.com>
2020-08-26 00:08:54 +0200

    Add react-navigation tests

---
+++ template/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'react-native',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  setupFiles: [
+    '<rootDir>/setupTests.js'
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect'
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)'
+  ]
+}
