7c6f10d87862e2e701e79b8f910ec96a6f5a9c1b
Kacper Grzeszczyk <kacgrzes@gmail.com>
2020-08-26 12:40:43 +0200

    Make eslint work correctly

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  arrowParens: 'avoid',
+  insertPragma: false,
+  jsxSingleQuote: false,
+}
